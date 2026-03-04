d142285808aae0d92867cfa684b3b4e04b362e47
DarthKarim <darthkarim@proton.me>
2026-03-04 13:48:14 +0100

    Added plugin to fix pages SPA issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    githubPagesSpa({ verbose: true }),
   ],
 })
