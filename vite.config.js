8c6019b3c656c2ecf95fc405cd7e8b38925e10c6
DarthKarim <darthkarim@proton.me>
2026-03-04 13:59:09 +0100

    Fixed base config (again)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/triplemantaweb',
+  base: '/triplemantaweb/',
   plugins: [
     react(),
     githubPagesSpa({ verbose: true }),
