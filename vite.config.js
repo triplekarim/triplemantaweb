ffc8937eeaefe22d18f52d4350f3f0f916462c4b
DarthKarim <darthkarim@proton.me>
2026-03-04 14:22:09 +0100

    Reverted some more changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    githubPagesSpa({ verbose: true }),
   ],
 })
