e6ee94c8bfa04d717f886f7cdb7b296e1f60b85a
DarthKarim <darthkarim@proton.me>
2026-03-30 19:54:55 +0200

    Added some text to about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -11,9 +11,12 @@ export default function Imprint() {
 
         <h1>About Us</h1>
 
-        <h2>Meet the faces that empower your game</h2>
+        <h2>Our Products at a glance</h2>
 
-        <p>This site is still under construction.</p>
+        <p><b>Hey there!</b> Are you looking for roblox products with tons of features and customization you could just drag-n-drop into your game? 
+          Then you're in the right place! Feel free to check out our free Triton Framework with tons of included modules that make your life easier. 
+          If you need even more tripleManta greatness, we have bundles available for purchase. Just visit our <a href="https://triplemanta.lemonsqueezy.com/">storefront</a>!
+        </p>
       </div>
     </div>
   );
